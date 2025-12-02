493e1734a897bab7ccce3132eaacba8011b872af
PSDT707 <35184132+Dazmed707@users.noreply.github.com>
2025-12-02 07:22:37 -0500

    Add tmux and screen to dependency installation

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,6 @@
 #Docker stuff
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg tmux screen
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
